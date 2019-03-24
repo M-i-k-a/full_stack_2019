0ee0a5b9969e5e60350b7d2d616c6c43b269533b
Mika Kaukolinna <mika.kaukolinna@gofore.com>
2019-03-24 19:20:21 +0200

    1.5 done

--- osa1/1_kurssitiedot/src/index.js
+++ osa1/1_kurssitiedot/src/index.js
@@ -40,27 +40,29 @@ const Part = (props) => {
 }
 
 const App = () => {
-    const course = 'Half Stack -sovelluskehitys'
-    const parts = [
-    {
-        name: 'Reactin perusteet',
-        exercises: 10
-    },
-    {
-        name: 'Tiedonvälitys propseilla',
-        exercises: 7
-    },
-    {
-        name: 'Komponenttien tila',
-        exercises: 14
-    }
-    ]
+    const course = {
+        name: 'Half Stack -sovelluskehitys',
+        parts: [
+          {
+            name: 'Reactin perusteet',
+            exercises: 10
+          },
+          {
+            name: 'Tiedonvälitys propseilla',
+            exercises: 7
+          },
+          {
+            name: 'Komponenttien tila',
+            exercises: 14
+          }
+        ]
+      }
 
     return (
         <div>
-            <Header course={course}/>
-            <Content parts={parts}/>
-            <Total total={parts}/>
+            <Header course={course.name}/>
+            <Content parts={course.parts}/>
+            <Total total={course.parts}/>
         </div>
     )
 }
