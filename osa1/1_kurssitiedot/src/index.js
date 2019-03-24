cb13ad92ba24b44f3b64ab9a67b527ca54f136b8
Mika Kaukolinna <mika.kaukolinna@gofore.com>
2019-03-24 18:22:28 +0200

    1.3 done

--- osa1/1_kurssitiedot/src/index.js
+++ osa1/1_kurssitiedot/src/index.js
@@ -11,11 +11,12 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+    console.log(props)
     return (
         <div>
-            <Part part={props.parts.part1} exercise={props.parts.exercises1}/>
-            <Part part={props.parts.part2} exercise={props.parts.exercises2}/>
-            <Part part={props.parts.part3} exercise={props.parts.exercises3}/>
+            <Part part={props.part1.name} exercise={props.part1.exercises}/>
+            <Part part={props.part2.name} exercise={props.part2.exercises}/>
+            <Part part={props.part3.name} exercise={props.part3.exercises}/>
         </div>
       )
 }
@@ -40,19 +41,25 @@ const Part = (props) => {
 
 const App = () => {
     const course = 'Half Stack -sovelluskehitys'
-    const part1 = 'Reactin perusteet'
-    const exercises1 = 10
-    const part2 = 'Tiedonvälitys propseilla'
-    const exercises2 = 7
-    const part3 = 'Komponenttien tila'
-    const exercises3 = 14
-    const parts = {part1, part2, part3, exercises1, exercises2, exercises3}
+    const part1 = {
+        name: 'Reactin perusteet',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'Komponenttien tila',
+        exercises: 14
+    }
+
 
     return (
         <div>
             <Header course={course} />
-            <Content parts={parts} />
-            <Total total={exercises1 + exercises2 + exercises3}/>
+            <Content part1={part1} part2={part2} part3={part3}/>
+            <Total total={part1.exercises + part2.exercises + part3.exercises}/>
         </div>
     )
 }
